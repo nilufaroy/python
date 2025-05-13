7022d03576560dc5e90a8924ff9d6dfe67ec3f86
nilufar <n.ibrohimjonova@newuu.uz>
2025-05-14 00:16:28 +0500

    changed string2

---
+++ lesson-2/homework/string2.py
@@ -0,0 +1,7 @@
+txt = 'LMaasleitbtui'
+
+lasetti = txt[0] + txt[2] + txt[4] + txt[6] + txt[8] + txt[10] + txt[12]  
+malibu  = txt[1] + txt[3] + txt[5] + txt[7] + txt[9] + txt[11]            
+
+print("Lasetti:", lasetti)
+print("Malibu:", malibu)
